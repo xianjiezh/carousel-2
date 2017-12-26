d4c6de53198bb08fed19d7ccaf6b2bd4afe781f0
xianjiezh <904292766@qq.com>
2017-12-26 19:51:56 +0800

    完成初步模型

--- 最终版/main.js
+++ 最终版/main.js
@@ -1,43 +1,38 @@
 
-$('.picsWrapper>.pics').children().eq(0).addClass('toCurrent')
-$('.picsWrapper>.pics').children().eq(1).addClass('toRight')
-$('.picsWrapper>.pics').children().eq(2).addClass('toRight')
-$('.picsWrapper>.pics').children().eq(3).addClass('toRight')
-log($('.picsWrapper>.pics').children().eq(3))
-function log(){
-    return console.log.apply(console,arguments)
+function log() {
+    return console.log.apply(console, arguments)
 }
 
 
-setInterval(function(){
-    setTimeout(function(){
-        $('.picsWrapper>.pics').children().eq(0).removeClass('toCurrent').addClass('toLeft')
-        $('.picsWrapper>.pics').children().eq(0).on('transitionend',function(){
-            $('.picsWrapper>.pics').children().eq(0).removeClass('toLeft').addClass('toRight')
-        })
-        $('.picsWrapper>.pics').children().eq(1).removeClass('toRight').addClass('toCurrent')
-    },2000)
-    setTimeout(function(){
-        $('.picsWrapper>.pics').children().eq(1).removeClass('toCurrent').addClass('toLeft')
-        $('.picsWrapper>.pics').children().eq(1).on('transitionend',function(){
-            $('.picsWrapper>.pics').children().eq(1).removeClass('toLeft').addClass('toRight')
-        })
-        $('.picsWrapper>.pics').children().eq(2).removeClass('toRight').addClass('toCurrent')
-    },4000)
-    setTimeout(function(){
-        $('.picsWrapper>.pics').children().eq(2).removeClass('toCurrent').addClass('toLeft')
-        $('.picsWrapper>.pics').children().eq(2).on('transitionend',function(){
-            $('.picsWrapper>.pics').children().eq(2).removeClass('toLeft').addClass('toRight')
-        })
-        $('.picsWrapper>.pics').children().eq(3).removeClass('toRight').addClass('toCurrent')
-    },6000)
-    setTimeout(function(){
-        $('.picsWrapper>.pics').children().eq(3).removeClass('toCurrent').addClass('toLeft')
-        $('.picsWrapper>.pics').children().eq(3).on('transitionend',function(){
-            $('.picsWrapper>.pics').children().eq(3).removeClass('toLeft').addClass('toRight')
-        })
-        $('.picsWrapper>.pics').children().eq(0).removeClass('toRight').addClass('toCurrent')
-    },8000)
-},8000)
-
-
+setTimeout(function () {
+    $('.picsWrapper>.pics').children().eq(0).removeClass('toCurrent').addClass('toLeft')
+    $('.picsWrapper>.pics').children().eq(1).removeClass('toRight').addClass('toCurrent')
+    log('1',$('.picsWrapper>.pics').children().eq(0))
+    $('.picsWrapper>.pics').children().eq(0).one('transitionend', function () {
+        $('.picsWrapper>.pics').children().eq(0).removeClass('toLeft').addClass('toRight')
+    })
+}, 2000)
+setTimeout(function () {
+    $('.picsWrapper>.pics').children().eq(1).removeClass('toCurrent').addClass('toLeft')
+    $('.picsWrapper>.pics').children().eq(2).removeClass('toRight').addClass('toCurrent')
+    log('2',$('.picsWrapper>.pics').children().eq(0))
+    $('.picsWrapper>.pics').children().eq(1).one('transitionend', function () {
+        $('.picsWrapper>.pics').children().eq(1).removeClass('toLeft').addClass('toRight')
+    })
+}, 4000)
+setTimeout(function () {
+    $('.picsWrapper>.pics').children().eq(2).removeClass('toCurrent').addClass('toLeft')
+    $('.picsWrapper>.pics').children().eq(3).removeClass('toRight').addClass('toCurrent')
+    log('3',$('.picsWrapper>.pics').children().eq(0))
+    $('.picsWrapper>.pics').children().eq(2).one('transitionend', function () {
+        $('.picsWrapper>.pics').children().eq(2).removeClass('toLeft').addClass('toRight')
+    })
+}, 6000)
+setTimeout(function () {
+    $('.picsWrapper>.pics').children().eq(3).removeClass('toCurrent').addClass('toLeft')
+    $('.picsWrapper>.pics').children().eq(0).removeClass('toRight').addClass('toCurrent')
+    log('4',$('.picsWrapper>.pics').children().eq(0))
+    $('.picsWrapper>.pics').children().eq(3).one('transitionend', function () {
+        $('.picsWrapper>.pics').children().eq(3).removeClass('toLeft').addClass('toRight')
+    })
+}, 8000)
