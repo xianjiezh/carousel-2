e244905e9dca9dd49e7c27a5ce52901d5885d4f9
xianjiezh <904292766@qq.com>
2017-12-26 21:28:13 +0800

    添加移动按键

--- 最终版/main.js
+++ 最终版/main.js
@@ -2,9 +2,23 @@
 function log() {
     return console.log.apply(console, arguments)
 }
-let len = $('img').length
+
 let n = 0
 setInterval(function () {
+    moveRight(n)
+    n += 1
+}, 5000)
+
+function getN(num) {
+    if (num == len) {
+        num = 0
+    }
+    return num
+}
+
+let len = $('img').length
+// 图片右移
+function moveRight(n){
     n = n % len
     let n1 = getN(n)
     let n2 = getN(n+1)
@@ -13,12 +27,12 @@ setInterval(function () {
     $('.picsWrapper>.pics').children().eq(n1).one('transitionend', function () {
         $('.picsWrapper>.pics').children().eq(n1).removeClass('toLeft').addClass('toRight')
     })
-    n += 1
-}, 2000)
-
-function getN(num) {
-    if (num == len) {
-        num = 0
-    }
-    return num
 }
+function moveLeft(n){
+
+}
+var right = document.querySelector('.right')
+right.addEventListener('click',function () {
+    moveRight(n)
+    n += 1
+})
