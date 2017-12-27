884066c21a4ecc2e8fac219e2a1b2d85e55e2a64
xianjiezh <904292766@qq.com>
2017-12-27 22:31:24 +0800

    添加右滑

--- 最终版/main.js
+++ 最终版/main.js
@@ -4,11 +4,12 @@ function log() {
 }
 
 let n = 0
-setInterval(function () {
-    moveRight(n)
+let timer = setInterval(function () {
+    moveLeft(n)
     n += 1
-}, 5000)
+}, 5500)
 
+let len = $('img').length
 function getN(num) {
     if (num == len) {
         num = 0
@@ -16,23 +17,38 @@ function getN(num) {
     return num
 }
 
-let len = $('img').length
 // 图片右移
-function moveRight(n){
+function moveLeft(n) {
     n = n % len
     let n1 = getN(n)
-    let n2 = getN(n+1)
+    let n2 = getN(n + 1)
     $('.picsWrapper>.pics').children().eq(n1).removeClass('toCurrent').addClass('toLeft')
     $('.picsWrapper>.pics').children().eq(n2).removeClass('toRight').addClass('toCurrent')
     $('.picsWrapper>.pics').children().eq(n1).one('transitionend', function () {
         $('.picsWrapper>.pics').children().eq(n1).removeClass('toLeft').addClass('toRight')
     })
 }
-function moveLeft(n){
-
+function moveRight(n) {
+    n = n % len
+    let n1 = getN(n)
+    let n2 = getN(n + 1)
+    let current = $('img.toCurrent')
+    log(current)
+    let left = $('img.toLeft')
+    $('img').one('transitionend',function(){
+        current.removeClass('toCurrent').addClass('toRight')
+        left.removeClass('toLeft').addClass('toCurrent')
+        current.one('transitionend',function(){
+            left.prev().removeClass('toRight').addClass('toLeft')
+        })
+    })
+    
 }
-var right = document.querySelector('.right')
-right.addEventListener('click',function () {
-    moveRight(n)
+$('.left').on('click', function () {
+    moveLeft(n)
     n += 1
+})
+$('.right').on('click', function () {
+    moveRight(n)
+    n -= 1
 })
