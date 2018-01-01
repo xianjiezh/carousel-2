6373920f5523097dff580bd946744395c55cb089
xianjiezh <904292766@qq.com>
2018-01-01 23:14:18 +0800

    添加jQuery

--- 最终版-2/main.js
+++ 最终版-2/main.js
@@ -1,12 +1,12 @@
 
-let n = 1
+let n = 0
 setInterval(function(){
     let left = -168 * n + 'px'
     $('.pictures').css({
         left:left
     })
     n ++
-    if (n === 5){
-
+    if (n === 4){
+        n = 0
     }
 },2000)
