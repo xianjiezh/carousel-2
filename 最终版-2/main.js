25681d583bca0625cede457b82d9c005053a8a75
xianjiezh <904292766@qq.com>
2018-01-04 22:31:17 +0800

    添加备份

--- 最终版-2/main.js
+++ 最终版-2/main.js
@@ -1,12 +1,14 @@
 
-let n = 0
-setInterval(function(){
-    let left = -168 * n + 'px'
-    $('.pictures').css({
-        left:left
-    })
-    n ++
-    if (n === 4){
-        n = 0
-    }
-},2000)
+function log(){
+    console.log.apply(console,arguments)
+}
+let timer = setInterval(function(){
+
+},1500)
+
+let $firstCopy = $('.pictures').children().eq(0).clone(true)
+$('.pictures').prepend($firstCopy)
+let $lastCopy = $('.pictures').children().eq(-1).clone(true)
+$('.pictures').append($lastCopy)
+log($lastCopy)
+log($firstCopy)
