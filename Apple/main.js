174af49952694b75956876564bdd06b04ec74157
xianjiezh <904292766@qq.com>
2018-01-18 21:17:35 +0800

    完成自动轮播

--- Apple/main.js
+++ Apple/main.js
@@ -1,18 +1,26 @@
 
-let ul = document.querySelector('main .menu ul')
+let li = document.querySelectorAll('main .menu ul li')
+for (let i = 0; i < li.length; i++) {
+    const pic = li[i]
+    console.log('pic', pic)
+    pic.addEventListener('click',function(e){
+        let siblings = e.currentTarget.parentElement.children
+        for (let i = 0; i < siblings.length; i++) {
+            const element = siblings[i];
+            element.classList.remove('active')
+        }
+        e.currentTarget.classList.add('active')
+    })
+}
 
-ul.addEventListener('click',function(e){
-    let li = e.currentTarget.children
-    for (let i = 0; i < li.length; i++) {
-        let l = li[i]
-        l.classList.remove('active')
-    }
-    if(e.target.tagName == 'IMG'){
-        e.target.parentNode.classList.add('active')
-    } else{
-        e.target.classList.add('active')
-    }
-    
-    // e.target.parentNode.children.classList.remove('active')
-    // e.target.classList.add('active')
-})
+
+
+let timer = setInterval(slideLeft, 3000)
+let n = 0
+function slideLeft(){
+    n %= 4
+    let slides = document.querySelector('main .wrapper ul')
+    let distance = -n * 920 +'px'
+    slides.style.marginLeft = distance
+    n += 1
+}
