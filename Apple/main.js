6fb981c317a46e27fa31d5b49998263f692f6bab
xianjiezh <904292766@qq.com>
2018-01-18 21:55:32 +0800

    完成函数的封装

--- Apple/main.js
+++ Apple/main.js
@@ -1,26 +1,38 @@
 
-let li = document.querySelectorAll('main .menu ul li')
-for (let i = 0; i < li.length; i++) {
-    const pic = li[i]
-    console.log('pic', pic)
+let n = 0
+
+let buttons = document.querySelectorAll('main .menu ul li')
+for (let i = 0; i < buttons.length; i++) {
+    const pic = buttons[i]
     pic.addEventListener('click',function(e){
-        let siblings = e.currentTarget.parentElement.children
-        for (let i = 0; i < siblings.length; i++) {
-            const element = siblings[i];
-            element.classList.remove('active')
-        }
-        e.currentTarget.classList.add('active')
+        removeAllActive(buttons)
+        n = nthSibling(buttons, e.currentTarget)
+        buttons[n].classList.add('active')
+        slideLeft()
     })
 }
 
-
-
 let timer = setInterval(slideLeft, 3000)
-let n = 0
 function slideLeft(){
     n %= 4
     let slides = document.querySelector('main .wrapper ul')
     let distance = -n * 920 +'px'
     slides.style.marginLeft = distance
+    removeAllActive(buttons)
+    buttons[n].classList.add('active')
     n += 1
+}
+function removeAllActive(element){
+    for (let i = 0; i < element.length; i++) {
+        const ele = element[i];
+        ele.classList.remove('active')
+    }
+}
+function nthSibling(siblings, target){
+    for (let i = 0; i < siblings.length; i++) {
+        let element = siblings[i];
+        if (element === target){
+            return i
+        }
+    }
 }
